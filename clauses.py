49fb734333f869114ffe9ad51f48ddd05cdfaabf
Melecio Ponte <pontezambrano@gmail.com>
2014-06-11 10:51:18 -0430

    New clause: Every row contains at least one number in range 1..9.

--- clauses.py
+++ clauses.py
@@ -1,6 +1,10 @@
 # General clause string
 clause = ""
+# This variable is for documenting purposes only. (MP)
 number = 0
+
+
+## Individual cell clauses
 
 # Row i, column j contains exactly one value from 1 to 9.
 # 81 clauses
@@ -18,5 +22,15 @@ for i in range(1, 10):
             for l in range(k+1, 10):
                 clause +=  '-' + `i` + `j` + `k` + ' ' + '-' + `i` + `j` + `l`
                 clause += ' 0 \n'
+
+## Row clauses
+
+# Row i has contains a value in range 1 to 9.
+# 81 clauses
+for i in range(1, 10):
+    for j in range(1, 10):
+        for k in range(1, 10):
+            clause += `i` + `k` + `j` + ' '
+        clause += '0 \n'
 
 print(clause)
