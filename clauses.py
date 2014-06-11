26901400f3200954070e6f57768798c06015916a
Matteo Ferrando <matteo.ferrando2@gmail.com>
2014-06-11 18:57:10 -0430

    3x3 blocks clauses done

--- clauses.py
+++ clauses.py
@@ -1,5 +1,7 @@
+#! /usr/bin/env python
+
 # General clause string
-clause = ""
+clause = ''
 # This variable is for documenting purposes only. (MP)
 number = 0
 
@@ -58,8 +60,36 @@ for i in range(1, 10):
     for j in range(1, 10):
         for k in range(1, 10):
             for l in range(k+1, 10):
-                clause +=  '-' + `k` + `i` + `j` + ' ' + '-' + `l` + `i` + `j`
-                clause += ' 0 \n'
+                clause += '-' + `k` + `i` + `j` + ' ' + '-' + `l` + `i` + `j` + ' '
+                clause += '0 \n'
 
+## Block clauses
+
+# Each number appears at least once in each block
+# 81 clauses
+for z in range(1,10):
+    for i in range(0,3):
+        for j in range(0,3):
+            for x in range(1,4):
+                for y in range(1,4):
+                    clause += `(3*i + x)` + `(3*j + y)` + `z` + ' '
+            clause += '0 \n'
+
+# Each number appears at most once in each block
+# 2916 clauses
+for z in range(1,10):
+    for i in range(0,3):
+        for j in range(0,3):
+            for x in range(1,4):
+                for y in range(1,4):
+                    for k in range(y+1,4):
+                        clause += '-' + `(3*i + x)` + `(3*j + y)` + `z` + ' '
+                        clause += '-' + `(3*i + x)` + `(3*j + k)` + `z` + ' '
+                        clause += '0 \n'
+                    for k in range(x+1,4):
+                        for l in range(1,4):
+                            clause += '-' + `(3*i + x)` + `(3*j + y)` + `z` + ' '
+                            clause += '-' + `(3*i + k)` + `(3*j + l)` + `z` + ' '
+                            clause += '0 \n'
 
 print(clause)
