04052a700b8ebe62749df6174612828fae25dce6
Melecio Ponte <pontezambrano@gmail.com>
2014-06-11 10:37:24 -0430

    New clause: Every cell contains only one value. Fixed: Number of clauses reported for the first clause was wrong.        (was 729 when 81 was the correct number)

--- clauses.py
+++ clauses.py
@@ -2,17 +2,21 @@
 clause = ""
 number = 0
 
-# Row i, column j contains exactly one value  from 1 to 10
-# 729 clauses
-
+# Row i, column j contains exactly one value from 1 to 9.
+# 81 clauses
 for i in range(1, 10):
     for j in range(1, 10):
         for k in range(1, 10):
             clause += `i` + `j` + `k` + ' '
         clause += '0 \n'
 
-
-
+# Only one value for every row i column j
+# 2916 clauses
+for i in range(1, 10):
+    for j in range(1, 10):
+        for k in range(1, 10):
+            for l in range(k+1, 10):
+                clause +=  '-' + `i` + `j` + `k` + ' ' + '-' + `i` + `j` + `l`
+                clause += ' 0 \n'
 
 print(clause)
-
